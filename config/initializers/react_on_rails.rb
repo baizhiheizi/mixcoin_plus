51362800d02ab38cb0e587e1519d8a94e1a6b94f
an-lee <an.lee.work@gmail.com>
2021-03-18 12:31:33 +0800

    chore: add rubocop

--- config/initializers/react_on_rails.rb
+++ config/initializers/react_on_rails.rb
@@ -25,7 +25,7 @@ ReactOnRails.configure do |config|
   #
   # Alternately, you can remove the `ReactOnRails::TestHelper.configure_rspec_to_compile_assets`
   # and set the config/webpacker.yml option for test to true.
-  config.build_test_command = "RAILS_ENV=test bin/webpack"
+  config.build_test_command = 'RAILS_ENV=test bin/webpack'
 
   ################################################################################
   ################################################################################
@@ -41,5 +41,5 @@ ReactOnRails.configure do |config|
   # different. You should have ONE server bundle which can create all of your server rendered
   # React components.
   #
-  config.server_bundle_js_file = "hello-world-bundle.js"
+  config.server_bundle_js_file = 'hello-world-bundle.js'
 end
