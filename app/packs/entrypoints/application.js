26292f9470b658610a634e51ca0402c685632bb5
an-lee <an.lee.work@gmail.com>
2021-03-18 11:56:38 +0800

    chore: config rails_on_react

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -2,12 +2,21 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+//
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import ReactOnRails from 'react-on-rails';
+import App from '../src/apps/application/App';
+import '../stylesheets/application.css';
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// This is how react_on_rails can see the HelloWorld in the browser.
+ReactOnRails.register({
+  App,
+});
---
+++ app/packs/src/apps/application/App.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function App(props: { page: string }) {
+  return <>Hello world {props.page} </>;
+}
