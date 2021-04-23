615a5a806cfee9fc4dab8d0f5ea71b0901143d1f
an-lee <an.lee.work@gmail.com>
2021-04-23 09:00:54 +0800

    fix(book): fix order book when ws msg update

--- app/packs/apps/application/pages/ExchangePage/components/BookComponent.tsx
+++ app/packs/apps/application/pages/ExchangePage/components/BookComponent.tsx
@@ -1,22 +1,21 @@
 import { useWebSocket } from 'ahooks';
-import { ITick, ITrade, WS_ENDPOINT } from 'apps/application/utils';
+import { orderBookMessageReducer } from 'apps/application/reducers';
+import {
+  BigNumberConfig,
+  ITrade,
+  parseBookNumber,
+  WS_ENDPOINT,
+} from 'apps/application/utils';
 import { ReadyState } from 'apps/shared';
 import BigNumber from 'bignumber.js';
 import { Market } from 'graphqlTypes';
 import pako from 'pako';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useTranslation } from 'react-i18next';
 import { v4 as uuid } from 'uuid';
 import { ActivityIndicator } from 'zarm';
 
-BigNumber.config({
-  FORMAT: {
-    decimalSeparator: '.',
-    groupSeparator: ',',
-    groupSize: 3,
-    secondaryGroupSize: 0,
-  },
-});
+BigNumber.config(BigNumberConfig);
 
 export default function BookComponent(props: {
   market: Partial<Market>;
@@ -28,131 +27,16 @@ export default function BookComponent(props: {
 }) {
   const { t } = useTranslation();
   const { market, setOrderPrice, setOrderAmount, ticker, setTimestamp } = props;
-  const [connected, setConnected] = useState(false);
-  const [book, setBook] = useState<{
-    asks: ITick[];
-    bids: ITick[];
-  }>({ asks: [], bids: [] });
+  const [book, dispatchBook] = useReducer(orderBookMessageReducer, {
+    asks: [],
+    bids: [],
+  });
 
-  function handleOrderOpenOnBook(tick: ITick) {
-    const price = new BigNumber(tick.price);
-    const amount = new BigNumber(tick.amount);
-    const asks = [...book.asks];
-    const bids = [...book.bids];
-
-    if (tick.side === 'ASK') {
-      for (let i = 0; i < book.asks.length; i++) {
-        const bo = book.asks[i];
-        const bp = new BigNumber(bo.price);
-        if (bp.isEqualTo(price)) {
-          bo.amount = new BigNumber(bo.amount).plus(amount).toFixed(4);
-          return;
-        }
-        if (bp.isGreaterThan(price)) {
-          book.asks.splice(i, 0, tick);
-          return;
-        }
-      }
-      asks.push(tick);
-    } else if (tick.side === 'BID') {
-      for (let i = 0; i < book.bids.length; i++) {
-        const bo = book.bids[i];
-        const bp = new BigNumber(bo.price);
-        if (bp.isEqualTo(price)) {
-          bo.amount = new BigNumber(bo.amount).plus(amount).toFixed(4);
-          return;
-        }
-        if (bp.isLessThan(price)) {
-          book.bids.splice(i, 0, tick);
-          return;
-        }
-      }
-      bids.push(tick);
-    }
-    setBook({ asks, bids });
-    setTimestamp(Date.now());
-  }
-
-  function handleOrderRemoveFromBook(tick: ITick) {
-    const price = new BigNumber(tick.price);
-    const amount = new BigNumber(tick.amount);
-
-    const asks = [...book.asks];
-    const bids = [...book.bids];
-    if (tick.side === 'BID') {
-      const index = bids.findIndex((bid) =>
-        new BigNumber(bid.price).isEqualTo(price),
-      );
-      if (index > -1) {
-        bids[index].amount = new BigNumber(bids[index].amount)
-          .minus(amount)
-          .toFixed(4);
-        if (bids[index].amount === '0.0000') {
-          bids.splice(index, 1);
-        }
-      }
-    } else if (tick.side === 'ASK') {
-      const index = asks.findIndex((ask) =>
-        new BigNumber(ask.price).isEqualTo(price),
-      );
-      if (index > -1) {
-        asks[index].amount = new BigNumber(asks[index].amount)
-          .minus(amount)
-          .toFixed(4);
-        if (asks[index].amount === '0.0000') {
-          asks.splice(index, 1);
-        }
-      }
-    }
-
-    setBook({ asks, bids });
-    setTimestamp(Date.now());
-  }
-
-  function handleMessage(raw: string) {
-    let msg: any;
-    try {
-      msg = JSON.parse(raw);
-    } catch {
-      msg = {};
-    }
-    setConnected(true);
-    switch (msg.data.event) {
-      case 'BOOK-T0':
-        const { asks, bids } = msg.data.data;
-        setBook({
-          asks: [...book.asks, ...asks.slice(0, 1000)],
-          bids: [...book.bids, ...bids.slice(0, 1000)],
-        });
-        break;
-      case 'HEARTBEAT':
-        return;
-      case 'ORDER-OPEN':
-        handleOrderOpenOnBook(msg.data.data);
-        break;
-      case 'ORDER-CANCEL':
-        handleOrderRemoveFromBook(msg.data.data);
-        break;
-      case 'ORDER-MATCH':
-        handleOrderRemoveFromBook(msg.data.data);
-        break;
-    }
-  }
-
-  const { sendMessage, readyState } = useWebSocket(WS_ENDPOINT, {
-    onMessage: (event) => {
-      const fileReader = new FileReader();
-      fileReader.onload = (e: any) => {
-        const msg = pako.ungzip(new Uint8Array(e.target.result), {
-          to: 'string',
-        });
-        handleMessage(msg);
-      };
-      fileReader.readAsArrayBuffer(event.data);
-    },
+  const { sendMessage, readyState, latestMessage } = useWebSocket(WS_ENDPOINT, {
+    onMessage: () => {},
     onOpen: () => {},
     onError: (e) => {
-      console.log(e);
+      console.error(e);
     },
     reconnectLimit: Infinity,
     reconnectInterval: 500,
@@ -169,20 +53,24 @@ export default function BookComponent(props: {
     }
   }, [market, readyState]);
 
-  const parseNumber = (str: string) => {
-    const number = new BigNumber(str);
-    if (number.isLessThan(0.0001)) {
-      return number.toFixed(8);
-    } else if (number.isLessThan(0.01)) {
-      return number.toFixed(6);
-    } else if (number.isLessThan(1)) {
-      return number.toFixed(4);
-    } else if (number.isLessThan(100)) {
-      return number.toFixed(2);
-    } else {
-      return number.toFixed(0);
+  useEffect(() => {
+    if (latestMessage) {
+      const fileReader = new FileReader();
+      fileReader.onload = (e: any) => {
+        let msg: any = pako.ungzip(new Uint8Array(e.target.result), {
+          to: 'string',
+        });
+        try {
+          msg = JSON.parse(msg);
+        } catch {
+          msg = {};
+        }
+        dispatchBook(msg.data);
+        setTimestamp(Date.now());
+      };
+      fileReader.readAsArrayBuffer(latestMessage.data);
     }
-  };
+  }, [market, latestMessage]);
 
   return (
     <>
@@ -202,16 +90,18 @@ export default function BookComponent(props: {
                 key={index}
                 className='flex items-center justify-between'
                 onClick={() => {
-                  setOrderPrice(new BigNumber(ask.price).toFixed(8));
-                  setOrderAmount(new BigNumber(ask.amount).toFixed(8));
+                  setOrderPrice(new BigNumber(ask.price).toFixed(6));
+                  setOrderAmount(new BigNumber(ask.amount).toFixed(6));
                 }}
               >
-                <div className='text-red-500'>{parseNumber(ask.price)}</div>
-                <div className='text-gray-500'>{parseNumber(ask.amount)}</div>
+                <div className='text-red-500'>{parseBookNumber(ask.price)}</div>
+                <div className='text-gray-500'>
+                  {parseBookNumber(ask.amount)}
+                </div>
               </div>
             ))}
           </div>
-        ) : connected ? (
+        ) : readyState === ReadyState.Open ? (
           <div className='text-center text-red-500'>{t('no_ask_order')}</div>
         ) : (
           <div className='flex p-4'>
@@ -239,11 +129,11 @@ export default function BookComponent(props: {
                 setOrderAmount(new BigNumber(bid.amount).toFixed(8));
               }}
             >
-              <div className='text-green-500'>{parseNumber(bid.price)}</div>
-              <div className='text-gray-500'>{parseNumber(bid.amount)}</div>
+              <div className='text-green-500'>{parseBookNumber(bid.price)}</div>
+              <div className='text-gray-500'>{parseBookNumber(bid.amount)}</div>
             </div>
           ))
-        ) : connected ? (
+        ) : readyState === ReadyState.Open ? (
           <div className='text-center text-green-500'>{t('no_bid_order')}</div>
         ) : (
           <div className='flex p-4'>
