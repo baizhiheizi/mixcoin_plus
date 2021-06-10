442df3f7b23a131d2962c1fa00f67171d508bd39
an-lee <an.lee.work@gmail.com>
2021-06-10 18:25:49 +0800

    fix(snapshot): ignore arbitrage charge snapshot

--- app/models/mixin_network_snapshot.rb
+++ app/models/mixin_network_snapshot.rb
@@ -89,7 +89,7 @@ class MixinNetworkSnapshot < ApplicationRecord
   def process!
     return if processed?
 
-    raise 'No Processor Implemented!' unless data.to_s.downcase.match?(/fee|commission|withraw/) || data.blank? || user_id == MixcoinPlusBot.api.client_id
+    raise 'No Processor Implemented!' unless data.to_s.downcase.match?(/fee|commission|withraw|arbitrage/) || data.blank? || user_id == MixcoinPlusBot.api.client_id
 
     process_booking_order_activit_bonus_snapshot
 
