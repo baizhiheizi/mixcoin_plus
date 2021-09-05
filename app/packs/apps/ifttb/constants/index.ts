5430df32e0f29426280b45163742347e21c1ff78
an-lee <an.lee.work@gmail.com>
2021-09-05 21:14:23 +0800

    refactor: rename

--- app/packs/apps/ifttb/constants/index.ts
+++ app/packs/apps/ifttb/constants/index.ts
@@ -1,4 +1,6 @@
-export const FSwapActionThemeColor = '#F0829D';
-export const FSwapLogoUrl = 'https://4swap.org/logo.png';
+export const FoxSwapActionThemeColor = '#F0829D';
+export const FoxSwapLogoUrl = 'https://4swap.org/logo.png';
+export const FoxSwapAppId = '6a4a121d-9673-4a7e-a93e-cb9ca4bb83a2'
 export const MixSwapActionThemeColor = '#8c2de7';
 export const MixSwapLogoUrl = 'https://mixin-images.zeromesh.net/w73fbJcibxozqgv44ZqtsxNmD0e8-eMXITnTil4S-zvw2xv8zzYdA5lnBH4i_nJlckiGWPqp8N93rLQ5_E7traThKH1M2jolq-R5=s256';
+export const MixSwapAppId = '6a4a121d-9673-4a7e-a93e-cb9ca4bb83a2'
