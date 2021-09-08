2ade7fb79d9a7ac68d51d8f7c9ae600ed466c0de
an-lee <an.lee.work@gmail.com>
2021-09-08 09:34:07 +0800

    fix(types): typo

--- app/graphql/types/applet_mix_swap_action_type.rb
+++ app/graphql/types/applet_mix_swap_action_type.rb
@@ -2,7 +2,7 @@
 
 module Types
   class AppletMixSwapActionType < Types::AppletActionType
-    field :params, Types::Applet4swapActionParamsType, null: false
+    field :params, Types::AppletMixSwapActionParamsType, null: false
     field :pay_asset, Types::MixinAssetType, null: false
     field :fill_asset, Types::MixinAssetType, null: false
   end
