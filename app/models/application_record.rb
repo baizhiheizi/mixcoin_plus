6004383082c18734d345c2bc7589b5eafbfa1406
an-lee <an.lee.work@gmail.com>
2023-03-17 12:36:45 +0800

    deploy to fly.io

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,4 +3,20 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   self.implicit_order_column = 'created_at'
+
+  def self.ransackable_attributes(_auth_object = nil)
+    column_names + _ransackers.keys
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    reflect_on_all_associations.map { |a| a.name.to_s }
+  end
+
+  def self.ransortable_attributes(auth_object = nil)
+    ransackable_attributes(auth_object)
+  end
+
+  def self.ransackable_scopes(_auth_object = nil)
+    []
+  end
 end
